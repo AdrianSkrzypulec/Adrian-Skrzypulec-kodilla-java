90eff858de0636604591eb15fd3bf996d01d32b8
AdrianSkrzypulec <adrian.skrzypulec@gmail.com>
2021-04-11 18:38:24 +0200

    bean ForumUser

--- kodilla-spring/src/test/java/com/kodilla/kodillaspring/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/kodillaspring/KodillaSpringApplicationTests.java
@@ -2,6 +2,7 @@ package com.kodilla.kodillaspring;
 
 import com.kodilla.spring.shape.Circle;
 import com.kodilla.spring.shape.Shape;
+import com.kodilla.spring.shape.Square;
 import com.kodilla.spring.shape.Triangle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,10 +19,8 @@ class KodillaSpringApplicationTests {
         //Given
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
         Shape shape = context.getBean(Circle.class);
-
         //When
         String name = shape.getShapeName();
-
         //Then
         assertEquals("This is a circle.", name);
     }
@@ -31,11 +30,21 @@ class KodillaSpringApplicationTests {
         //Given
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
         Shape shape = context.getBean(Triangle.class);
-
         //When
         String name = shape.getShapeName();
-
         //Then
         assertEquals("This is a triangle.", name);
     }
+
+    @Test
+    void testSquareLoadedIntoContainer() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Square shape = context.getBean(Square.class);
+        //When
+        String name = shape.getShapeName();
+        //Then
+        assertEquals("This is a square.", name);
+    }
+
 }
