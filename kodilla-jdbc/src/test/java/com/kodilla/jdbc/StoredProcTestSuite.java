fadd16f4988e84a03cf9cd834dd4f8bcc59f1cc3
AdrianSkrzypulec <adrian.skrzypulec@gmail.com>
2021-10-14 22:27:54 +0200

    Szukamy laptopów na eBay

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -31,53 +31,25 @@ public class StoredProcTestSuite {
         assertEquals(0, howMany);
     }
 
-    @Test
-    void testUpdateBestsellers() throws SQLException {
-        // Given
-        DbManager dbManager = DbManager.getInstance();
-        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=null";
-        Statement statement = dbManager.getConnection().createStatement();
-        statement.executeUpdate(sqlUpdate);
-
-        // When
-        String sqlProcedureCall = "CALL UpdateBestsellers()";
-        statement.execute(sqlProcedureCall);
-
-        // Then
-        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=null";
-        int howMany = -1;
-        ResultSet rs = statement.executeQuery(sqlCheckTable);
-        if (rs.next()) {
-            howMany = rs.getInt("HOW_MANY");
-        }
-        assertEquals(0, howMany);
-    }
-}
-
 //    @Test
-//    public void testUpdateVipLevels() throws SQLException {
+//    void testUpdateBestsellers() throws SQLException {
 //        // Given
 //        DbManager dbManager = DbManager.getInstance();
-//        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+//        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=null";
 //        Statement statement = dbManager.getConnection().createStatement();
 //        statement.executeUpdate(sqlUpdate);
-
-//        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
-//        ResultSet rs = statement.executeQuery(sqlCheckTable);
 //
 //        // When
-//        String sqlProcedureCall = "CALL UpdateVipLevels()";
+//        String sqlProcedureCall = "CALL UpdateBestsellers()";
 //        statement.execute(sqlProcedureCall);
 //
-//        System.out.println("sqlProcedureCall: " +sqlProcedureCall);
-//        System.out.println("rs: " +rs);
-//
 //        // Then
+//        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=null";
 //        int howMany = -1;
+//        ResultSet rs = statement.executeQuery(sqlCheckTable);
 //        if (rs.next()) {
 //            howMany = rs.getInt("HOW_MANY");
 //        }
 //        assertEquals(0, howMany);
-//        rs.close();
-//        statement.close();
 //    }
+}
